004f8203d44b1881c22a36065e8fc8663b4933d7
syyyym <gabi.lupusor@gmail.com>
2019-06-28 14:57:40 +0000

    get host from /config module, set it in express config settings table

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express = require('express'), 
-    path = require('path')
+    path = require('path'),
+    config = require('./config/config.js')
 var app = express(); 
 app.set('views', path.join(__dirname, 'views')); 
 app.engine('html', require('hogan-express')); 
 app.set('view engine', 'html');
+app.set('host', config.host); 
 app.use(express.static(path.join(__dirname, 'public'))); 
 app.set('port', process.env.PORT || 3000);
 
