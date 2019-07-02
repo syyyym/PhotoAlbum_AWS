826a008a666aa8d4d83b2d797fd2ebbe6c507bb7
syyyym <gabi.lupusor@gmail.com>
2019-07-02 10:57:12 +0000

    add /upload route handler

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,12 @@
 module.exports = function(express, app) { 
-
     var router = express.Router(); 
+        
     router.get('/', function(req, res, next){
-        res.render('index', {});
+        res.render('index',{host:app.get('host')}); 
     });
+    router.post('/upload', function(req, res, next){ 
+        // File upload
+    });
+
     app.use('/', router); 
 }
