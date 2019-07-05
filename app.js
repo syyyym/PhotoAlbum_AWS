d459bdb6c991a6f4f0c8a9e1d7a0dc1e734d26b2
syyyym <gabi.lupusor@gmail.com>
2019-07-05 16:21:30 +0000

    bring in formidable module and pass it in the /routes module

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-var express = require('express'), 
-    path = require('path'),
-    config = require('./config/config.js')
-var app = express(); 
-app.set('views', path.join(__dirname, 'views')); 
-app.engine('html', require('hogan-express')); 
+var express = require('express'),
+path = require('path'),
+config = require('./config/config.js'),
+fs = require('fs'), 
+os = require('os'), 
+formidable = require('formidable') 
+var app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('hogan-express'));
 app.set('view engine', 'html');
-app.set('host', config.host); 
-app.use(express.static(path.join(__dirname, 'public'))); 
+app.set('host', config.host);
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('port', process.env.PORT || 3000);
 
-require('./routes/routes.js')(express, app); 
-
-var server = require('http').createServer(app); 
+require('./routes/routes.js')(express, app, formidable, fs, os);
+var server = require('http').createServer(app);
 var io = require('socket.io')(server); 
 server.listen(app.get('port'), function(){
-    console.log('Photo Album running on port ' + app.get('port'));
+console.log('Photo Album running on port ' + app.get('port'));
 });
