1a8d6490ebfc9016fae085ef354a095b00a64603
syyyym <gabi.lupusor@gmail.com>
2019-07-16 11:34:56 +0000

    configure aws module for uploading to AWS S3 Bucket

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ fs = require('fs'),
 os = require('os'), 
 formidable = require('formidable'),
 gm = require('gm'),
-mongoose = require('mongoose') 
+mongoose = require('mongoose'),
+AWS = require('aws-sdk')
 
 mongoose.connect(config.dbURI, { useUnifiedTopology: true, useNewUrlParser: true });
 const db = mongoose.connection;
@@ -19,10 +20,17 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('hogan-express'));
 app.set('view engine', 'html');
 app.set('host', config.host);
+
+//configure aws s3
+var s3 = new AWS.S3({
+    accessKeyId: config.S3AccessKey,
+    secretAccessKey: config.S3Secret
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('port', process.env.PORT || 3000);
 
-require('./routes/routes.js')(express, app, formidable, fs, os, gm, mongoose, io);
+require('./routes/routes.js')(express, app, formidable, fs, os, gm, s3, mongoose, io, config);
 var server = require('http').createServer(app);
 var io = require('socket.io')(server); 
 server.listen(app.get('port'), function(){
